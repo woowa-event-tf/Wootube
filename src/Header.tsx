59aa23204cb5842011ab2e6ac36b2a0486060369
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-09-26 12:51:23 +0900

    feat: Header 구현

---
+++ src/Header.tsx
@@ -0,0 +1,56 @@
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <header>
+        <Link to="/">
+          <img
+            src="https://user-images.githubusercontent.com/59258239/134791043-8c560929-59d8-49db-9068-71527e9bbe4a.png"
+            alt="유튜브 로고입니다."
+            width="32px"
+          />
+          <h1>WooTube</h1>
+        </Link>
+        <img
+          src="https://user-images.githubusercontent.com/59258239/134790952-5e0d4cfc-593f-420f-bc6b-1ae113165420.png"
+          alt="우테코 로고입니다."
+          width="190px"
+          height="65px"
+        />
+      </header>
+    </HeaderWrapper>
+  );
+};
+
+const HeaderWrapper = styled.div`
+  border-bottom: 1px solid #d1d1d1;
+
+  > header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    margin: 0 5rem;
+
+    > a {
+      display: flex;
+      align-items: center;
+
+      :link,
+      :visited {
+        color: #000000;
+        text-decoration: none;
+      }
+
+      h1 {
+        font-weight: 600;
+        font-size: 1.5rem;
+        margin: 0 0.5rem;
+      }
+    }
+  }
+`;
+
+export default Header;

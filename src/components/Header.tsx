b295aeb6b0200a0150458b397853709412de3a14
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-10-04 21:47:39 +0900

    refactor: header 그림자 삭제

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,7 +35,6 @@ const HeaderWrapper = styled.div`
   height: 8vh;
 
   z-index: 1;
-  box-shadow: 0 1px 3px rgba(75, 68, 68, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 
   > header {
     display: flex;

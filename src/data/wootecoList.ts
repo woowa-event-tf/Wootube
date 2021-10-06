abb0fdb64c7eaa31025374798275a3f1fca1fe69
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-10-07 01:26:16 +0900

    refactor: tecoble 오타 수정

--- src/data/wootecoList.ts
+++ src/data/wootecoList.ts
@@ -10,7 +10,7 @@ export const wootecoList = [
     imageUrl: 'https://avatars.githubusercontent.com/u/45747236?s=200&v=4',
   },
   {
-    name: 'Techble',
+    name: 'Tecoble',
     url: 'https://tecoble.techcourse.co.kr/',
     imageUrl:
       'data:image/vnd.microsoft.icon;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAjhJREFUeNrEV4uRgkAMzdmAlGAJdqDXgSVYgiXQgSVYgiWgFYAVYAdqBXv7kDAhhGVFPDOT8QObvPyzRDU55xLPqefSfY7KWkdCkmrlufs/yhnET/0l87ykCLper1QUBV0uF7rf79Xv5fJ5dLVa0Xq9pkgqPP9S7ZInrDx3m83GwSlJkrg0bR65LMucF149CzHObbdbV5ZRkUyJY3673arDWuBut6ve5N8QfjgcOgoAcL/fO++N5l1pQF9OUAPFv9xnFZRBQaRV1bsMBJ8wro8aAOx6iyFwDMFbQyCiPdBnKRi500cIF2Qgf4IAoMTKAXhGK7W8FUo+9oSVE9QqTm/JYrFoJRy7Dp8saKgKYLUmyMUzHQqKjWcoRyzWIOA5ywtkvaRjGsqPkCd0OOAFcC8AdrE82NcfYhjyLEOkgS0AyFSUjJXFY9nyMBoW00w259PpRN5FrYZ9Pp/pHYJMJp4Tj8ej+W+mD/BgkcNnapIyZ/Rl6gDAqJWkQzIFSZktAIiRdjlm/Dsk9wPOh/l8bgNA/OEBCcI3IPJlOEq5L0MzoVtLiyyT4/HYKZMpGxHKPNiI+rrVFK2YB54G1QHA1uqePXYYQQ53UjncgsOItxlrzr8yjq0uqtuzCQCKIRAcWjaGFhIL6GAOaPQAMXYlw0KrAehZE9wHUBUyfq8spagk9qQEAJkaQFAqlMr7QOxaDsUcTpwBa+W8lqexVn3iYvLy1QzXMbRUXM14drx1Nfv25ZS+fT3/E2AAsWMCdac8y9AAAAAASUVORK5CYII=',
